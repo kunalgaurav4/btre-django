fd9ac55775dd5896ee605fd2261c8e477d31b165
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-11-29 19:49:50 +0530

    re-init project

--- btre/settings.py
+++ btre/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3=*c^xm6yzt-#)(@**x*-cg4d7g7-lt!(k$#_dx1w6*=q(*hrs'
+SECRET_KEY = '75me7b_=6h@&xzwp&cqcel$$@((d^)d5g%g=41ehwa(8l5-0%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
