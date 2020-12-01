37fa02857adb016c4f0900ec2bf1351b59507800
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-12-01 20:19:00 +0530

    search page

--- btre/settings.py
+++ btre/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE =  'Asia/Calcutta'
 
 USE_I18N = True
 
---
+++ listings/choices.py
@@ -0,0 +1,49 @@
+
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': 'Rs.100,000',
+    '200000': 'Rs.200,000',
+    '300000': 'Rs.300,000',
+    '400000': 'Rs.400,000',
+    '500000': 'Rs.500,000',
+    '600000': 'Rs.600,000',
+    '700000': 'Rs.700,000',
+    '800000': 'Rs.800,000',
+    '900000': 'Rs.900,000',
+    '1000000': 'Rs.1M+',
+}
+
+state_choices = {
+    'Andra Pradesh': 'Andra Pradesh',
+    'Arunachal Pradesh': 'Arunachal Pradesh',
+    'Assam': 'Assam',
+    'Bihar': 'Bihar',
+    'Chhattisgarh': 'Chhattisgarh',
+    'G': 'Goa',
+    'GU': 'Gujarat',
+    'H': 'Haryana',
+    'HP': 'Himachal Pradesh',
+    'JAK': 'Jammu and Kashmir',
+    'JK': 'Jharkhand',
+    'RCH': 'Ranchi',
+    'KR': 'Karnataka',
+    'KRL': 'Kerala',
+    'MP': 'Madya Pradesh',
+    'BP':  'Bhopal',
+    'MP': 'Maharashtra',
+    'OD':  'Orissa',
+    'Tamil Nadu': 'Tamil Nadu'
+
+}
