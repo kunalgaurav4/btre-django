790d5f4ef4395c43dd629c2c0daa86ddf27fffed
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-12-03 11:42:38 +0530

    deploy

--- btre/settings.py
+++ btre/settings.py
@@ -105,6 +105,9 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 WHITENOISE_USE_FINDERS = True
 # Password validation
