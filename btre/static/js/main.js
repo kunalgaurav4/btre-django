19f14aafca6f43cef4d1b9ccbe505c1c66dde446
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-12-04 16:42:07 +0530

    fade out

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -2,5 +2,5 @@ const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
 setTimeout(function(){
-    $('#message').fadeout('slow');
-},3000);
+    $('#message').fadeOut('slow');
+},1500);
